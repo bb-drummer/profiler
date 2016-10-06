c8420b3585030c4d8be158bb427c1c2c6ee9c07e
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-10-06 14:47:43 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Profiler/Writer/HtmlTable.php
+++ src/Profiler/Writer/HtmlTable.php
@@ -30,24 +30,24 @@ class HtmlTable extends WriterAbstract
     public function get()
     {
         $table = "<table class=\"Profiler\" cellspacing=\"3\" cellpadding=\"3\" style=\"color: #000000; font-family: 'Verdana', 'Arial', 'Helvetica'; font-size: 10px; border: 0px dotted #00ffff; width: 100%;\" border=\"0\" >\n"
-               . "    <thead>\n"
-               . "        <tr>\n"
-               . "            <th class=\"spacer\" style=\"background-color: #ff9900;\" colspan=\"2\"></th>\n"
-               . "            <th class=\"time\" style=\"background-color: #ff9900;\" colspan=\"3\">Time</th>\n"
-               . "            <th class=\"memory\" style=\"background-color: #ff9966;\" colspan=\"3\">Memory</th>\n"
-               . "        </tr>\n"
-               . "        <tr>\n"
-               . "            <th></th><!-- <th class=\"index\" style=\"background-color: #f7c64a;\">#</th -->\n"
-               . "            <th></th><!-- th class=\"title\" style=\"background-color: #f7c64a;\">Title</th -->\n"
-               . "            <th class=\"time start\" style=\"background-color: #9999cc;\">Start</th>\n"
-               . "            <th class=\"time stop\" style=\"background-color: #9999ff;\">Stop</th>\n"
-               . "            <th class=\"time diff\" style=\"background-color: #cc99cc;\">Difference</th>\n"
-               . "            <th class=\"memory start\" style=\"background-color: #ffcc99;\">Start</th>\n"
-               . "            <th class=\"memory stop\" style=\"background-color: #ff9966;\">Stop</th>\n"
-               . "            <th class=\"memory diff\" style=\"background-color: #cc99cc;\">Difference</th>\n"
-               . "        </tr>\n"
-               . "    </thead>\n"
-               . "    <tbody>\n";
+                . "    <thead>\n"
+                . "        <tr>\n"
+                . "            <th class=\"spacer\" style=\"background-color: #ff9900;\" colspan=\"2\"></th>\n"
+                . "            <th class=\"time\" style=\"background-color: #ff9900;\" colspan=\"3\">Time</th>\n"
+                . "            <th class=\"memory\" style=\"background-color: #ff9966;\" colspan=\"3\">Memory</th>\n"
+                . "        </tr>\n"
+                . "        <tr>\n"
+                . "            <th></th><!-- <th class=\"index\" style=\"background-color: #f7c64a;\">#</th -->\n"
+                . "            <th></th><!-- th class=\"title\" style=\"background-color: #f7c64a;\">Title</th -->\n"
+                . "            <th class=\"time start\" style=\"background-color: #9999cc;\">Start</th>\n"
+                . "            <th class=\"time stop\" style=\"background-color: #9999ff;\">Stop</th>\n"
+                . "            <th class=\"time diff\" style=\"background-color: #cc99cc;\">Difference</th>\n"
+                . "            <th class=\"memory start\" style=\"background-color: #ffcc99;\">Start</th>\n"
+                . "            <th class=\"memory stop\" style=\"background-color: #ff9966;\">Stop</th>\n"
+                . "            <th class=\"memory diff\" style=\"background-color: #cc99cc;\">Difference</th>\n"
+                . "        </tr>\n"
+                . "    </thead>\n"
+                . "    <tbody>\n";
         
         $checkpoints = $this->getProfiler()->getCheckpoints();
         $application = null;
@@ -95,7 +95,7 @@ class HtmlTable extends WriterAbstract
         $rawStartTime   = $checkpoint->startTime;
         $rawStopTime    = $checkpoint->stopTime;
         $startTime      = number_format($rawStartTime - $appStartTime, $timeFloating, ',', '');
-        $stopTime       = number_format($rawStopTime - $appStartTime,  $timeFloating, ',', '');
+        $stopTime       = number_format($rawStopTime - $appStartTime, $timeFloating, ',', '');
         $diffTime       = number_format($rawStopTime - $rawStartTime, $timeFloating, ',', '');
         
         $memoryFloating = $profiler->getMemoryFloating();
@@ -108,15 +108,15 @@ class HtmlTable extends WriterAbstract
         $indention      = str_repeat('&nbsp;', $checkpoint->depth * 4);
         
         $row = "        <tr>\n"
-             . "            <th class=\"index\" style=\"background-color: #f7c64a; text-align: right;\">{$index}</th>\n"
-             . "            <td class=\"title\" style=\"background-color: #f7c64a;\">{$indention}{$checkpoint->title}</td>\n"
-             . "            <td class=\"time start\" style=\"background-color: #9999cc; text-align: right;\">{$startTime}s</td>\n"
-             . "            <td class=\"time stop\" style=\"background-color: #9999ff; text-align: right;\">{$stopTime} s</td>\n"
-             . "            <td class=\"time diff\" style=\"background-color: #cc99cc; text-align: right;\">{$diffTime} s</td>\n"
-             . "            <td class=\"memory start\" style=\"background-color: #ffcc99; text-align: right;\">{$startMemory}</td>\n"
-             . "            <td class=\"memory stop\" style=\"background-color: #ff9966; text-align: right;\">{$stopMemory}</td>\n"
-             . "            <td class=\"memory diff\" style=\"background-color: #cc99cc; text-align: right;\">{$diffMemory}</td>\n"
-             . "        </tr>\n";
+                . "            <th class=\"index\" style=\"background-color: #f7c64a; text-align: right;\">{$index}</th>\n"
+                . "            <td class=\"title\" style=\"background-color: #f7c64a;\">{$indention}{$checkpoint->title}</td>\n"
+                . "            <td class=\"time start\" style=\"background-color: #9999cc; text-align: right;\">{$startTime}s</td>\n"
+                . "            <td class=\"time stop\" style=\"background-color: #9999ff; text-align: right;\">{$stopTime} s</td>\n"
+                . "            <td class=\"time diff\" style=\"background-color: #cc99cc; text-align: right;\">{$diffTime} s</td>\n"
+                . "            <td class=\"memory start\" style=\"background-color: #ffcc99; text-align: right;\">{$startMemory}</td>\n"
+                . "            <td class=\"memory stop\" style=\"background-color: #ff9966; text-align: right;\">{$stopMemory}</td>\n"
+                . "            <td class=\"memory diff\" style=\"background-color: #cc99cc; text-align: right;\">{$diffMemory}</td>\n"
+                . "        </tr>\n";
         
         return $row;
     }
